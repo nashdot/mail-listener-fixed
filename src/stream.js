50a2a81a0ddd8547d548ada74a0137f941a72ef1
Stanislav Lesnikov <stan@nashdot.com>
2019-01-07 15:16:05 +0100

    Reformat code with ESLint

--- src/stream.js
+++ src/stream.js
@@ -1,16 +1,14 @@
 const { Transform } = require('stream');
 
 export default class EMLStream extends Transform {
-    
-    constructor() {
-        super();
-        this.buffer = new Buffer('');
-    }
+  constructor() {
+    super();
+    this.buffer = new Buffer('');
+  }
 
-    _transform(chunk, encoding, done) {
-        this.buffer = Buffer.concat([this.buffer, chunk]);
-        this.push(chunk);
-        return done();
-    }
-    
+  _transform(chunk, encoding, done) {
+    this.buffer = Buffer.concat([this.buffer, chunk]);
+    this.push(chunk);
+    return done();
+  }
 }
